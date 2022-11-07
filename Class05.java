3aefddfd9db00009d9a6ae6896d8c14d9cb41eb7
purpletina <113041666+purpletina@users.noreply.github.com>
2022-11-07 21:53:48 +0800

    Update Class05.java

--- Class05.java
+++ Class05.java
@@ -1,13 +1,13 @@
 interface Math {
 	public void show();
 
-	public void add(int a, int b); // 計算a+b
+	public void add(int a, int b); 
 
-	public void sub(int a, int b); // 計算a-b
+	public void sub(int a, int b); 
 
-	public void mul(int a, int b); // 計算a*b
+	public void mul(int a, int b); 
 
-	public void div(int a, int b); // 計算a/b
+	public void div(int a, int b); 
 }
 
 class Compute implements Math {
@@ -37,7 +37,7 @@ class Compute implements Math {
 public class  {
 	public static void main(String args[]) {
 		Compute cmp = new Compute();
-		cmp.mul(3, 5); // 計算3*5
-		cmp.show(); // 此行會回應"ans=15"字串
+		cmp.mul(3, 5); 
+		cmp.show(); 
 	}
 }
